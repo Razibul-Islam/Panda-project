3ddc71a6ba531ddd0be6ed2794d5e81248c7705e
Razibul Islam <razibulislam665@gmail.com>
2022-08-16 10:55:02 +0600

    hidden button added

--- panda.js
+++ panda.js
@@ -3,3 +3,23 @@ const h2Element = document.getElementsByTagName('h2');
 for (let i = 0; i < h2Element.length; i++) {
     h2Element[i].style.color = 'lightblue';
 }
+document.getElementById('bag').style.backgroundColor = 'tomato';
+
+const cardClass = document.getElementsByClassName('card');
+
+for(let i = 0; i < cardClass.length; i++){
+    cardClass[i].style.borderRadius = '30px'
+}
+
+let count = 0;
+function eventH(){
+    console.log(count++);
+}
+
+const btns = document. getElementsByClassName("btn");
+for (var i = 0; i < btns. length; i++) {
+btns[i]. addEventListener("click", function (event) {
+    // console.log('hello');
+    event.target.style.display = 'none'
+});
+}
